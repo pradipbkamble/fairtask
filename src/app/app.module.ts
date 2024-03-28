16c5f018ae955e0b1ef6053178b087cea6252638
pradipbkamble <pradipbkamble001@gmail.com>
2024-03-28 13:07:56 +0530

    fairtask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './components/comp/navbar/navbar.component';
+import { SinglefairComponent } from './components/comp/singlefair/singlefair.component';
+import { HomeComponent } from './components/comp/home/home.component';
+import { AboutComponent } from './components/comp/about/about.component';
+import { FairsCardComponent } from './components/comp/fairs-card/fairs-card.component';
+import { DashboardFairsComponent } from './components/comp/dashboard-fairs/dashboard-fairs.component';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SinglefairComponent,
+    HomeComponent,
+    AboutComponent,
+    FairsCardComponent,
+    DashboardFairsComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
